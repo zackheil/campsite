136bd171bb3bd5ee6e90fba17de9cf1578a06e4e
Zack Heil <57419337+zackheil@users.noreply.github.com>
2024-05-24 18:17:44 -0500

    Silence unzip output

--- assets/init.sh
+++ assets/init.sh
@@ -20,7 +20,7 @@ if [ ! -f "$zip_file" ]; then
   exit 1  
 fi
 
-unzip -qq $zip_file 2>&1 /dev/null
+unzip -qq $zip_file > /dev/null 2>&1
 
 dir_name="campsite-$branch"
 if [ ! -d "$dir_name" ]; then
