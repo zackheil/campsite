df482c513b30dead6a52a303e4f7167895a3ad5f
Zack Heil <57419337+zackheil@users.noreply.github.com>
2024-05-23 21:11:01 -0500

    Adds test script with debug logging

--- README.md
+++ README.md
@@ -8,4 +8,10 @@ Comes in three flavors (ideally in two OSes, but just macOS for now):
 - comfortable: includes commonly used programs used
 - full: used for brand new computer setups; includes everything
 
-![Campsite](.assets/campsite.jpg)
+![Campsite](assets/campsite.jpg)
+
+Setup with the following command:
+
+```zsh
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/zackheil/campsite/main/assets/init.sh)"
+```
---
+++ assets/init.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env zsh
+
+branch="dev"
+zip_file="campsite-$branch.zip"
+
+temp_dir=$(mktemp -d)
+
+# Ensure the temporary directory was created
+if [ ! -d "$temp_dir" ]; then
+  echo "Failed to create temporary directory."
+  exit 1
+fi
+
+echo "Temporary directory created at $temp_dir"
+
+# Change to the temporary directory
+cd "$temp_dir" || exit
+
+# Download the GitHub repository as a zip file
+curl -L -O https://github.com/zackheil/campsite/archive/$branch.zip
+
+# Check if the download was successful
+if [ ! -f $zip_file ]; then
+  echo "Failed to download the repository."
+  exit 1
+fi
+
+echo "Repository downloaded as $zip_file"
+
+# Unzip the contents
+unzip $zip_file
+
+# Check if the unzip was successful
+if [ ! -d "campsite-$branch" ]; then
+  echo "Failed to unzip the repository."
+  exit 1
+fi
+
+echo "Repository unzipped"
+
+# Remove any existing .installer directory
+rm -rf "$HOME/.installer"
+
+# Move the unzipped contents to the home directory as .installer
+mv campsite-$branch "$HOME/.installer"
+
+# Check if the move was successful
+if [ ! -d "$HOME/.installer" ]; then
+  echo "Failed to move the repository to ~/.installer."
+  exit 1
+fi
+
+echo "Repository moved to ~/.installer"
+echo "Temporary directory cleaned up"
+echo "Setup complete. The repository is now located at ~/.installer"
